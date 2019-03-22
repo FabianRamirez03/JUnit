fbaa472a7c4103813aa65ebd845c6639c2cf3467
[Fabian] <[ramirezfabian003@gmail.com]>
2019-03-22 14:16:52 -0600

    Add documentation to Mocktest

--- Test/MockitoTest.java
+++ Test/MockitoTest.java
@@ -2,6 +2,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+/*
+
+    Los MockObjects u objetos "falsos", son aquellos que
+brindan una simulación de lo que debería retornar
+una función o método, aunque el código de esta no
+esté implementado en el proyecto aún.
+    Es una herramienta sumamente poderosa ya que permite
+continuar el desarrollo de una programa aunque falten elementos
+fundamentales para su funcionamiento.
+    Los mock objects son muy utilizados a la hora de programar
+en conjunto a un equipo de trabajo ya que de esta forma, cada
+programador puede cumplir con su parte, sin depender directamente
+del trabajo de un compañero
+
+ */
+
+
+
+
 public class MockitoTest {
 
 
